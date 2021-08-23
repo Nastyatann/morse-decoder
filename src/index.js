4d47c9f4945574508f5c37179d8647da5b76a3fe
nastyatann <anastasia.kuz.84@gmail.com>
2021-08-23 16:07:42 +0300

    feat: add morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [];
+    let lettersDecode = [];
+    let str = '';
+    let n = expr.length;
+
+    for (let i = 0; i < n; i++) {
+        if (expr[i] === '*') {
+            letters.push(' ');
+            i += 10;
+        }
+        if (expr[i] === '1') {
+            if (expr[i + 1] === '0') {
+                str += '.';
+            } else {
+                str += '-';
+            }
+            i++;
+            if ((i + 1) % 10 === 0) {
+                letters.push(str);
+                str = '';
+            }
+        }
+    }
+    for (let j = 0; j < letters.length; j++) {
+        if (letters[j] != ' ') {
+            lettersDecode[j] = MORSE_TABLE[letters[j]];
+        } else {
+            lettersDecode.push(' ');
+        }
+    }
+    return  lettersDecode.join('');
 }
+
+
 
 module.exports = {
     decode
